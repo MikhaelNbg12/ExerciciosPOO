70502059094e20fd4e15848413bbabdd73079fe6
Mikhael Nóbrega <mikhael.nobrega12@gmail.com>
2024-03-19 16:15:26 -0300

    alteração 1

--- Aula5/Aula5/src/Exercícios_Nota1/Main.java
+++ Aula5/Aula5/src/Exercícios_Nota1/Main.java
@@ -2,8 +2,8 @@ package Exercícios_Nota1;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa a = new Pessoa("João", "Masculino", 7, false);
-        Pessoa b = new Pessoa("Maria", "Feminino", 14, true);
+        Pessoa a = new Pessoa("Mikhael", "Masculino", 22, false);
+        Pessoa b = new Pessoa("Thatiane", "Feminino", 20, true);
 
         Churrasco churrasco = new Churrasco();
         churrasco.verificarConsumo(a);
--- Aula5/Aula5/src/Exercícios_Nota2/Main.java
+++ Aula5/Aula5/src/Exercícios_Nota2/Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main (String[] args) {
    
     Lampada lampada = new Lampada();
-    System.out.println("Estado inicial: " + lampada.checaEstado());
+    System.out.println("Lampada começou: " + lampada.checaEstado());
     lampada.click(); 
-    System.out.println("Após 1º click: " + lampada.checaEstado());
+    System.out.println("Apos primeiro click: " + lampada.checaEstado());
     System.out.println("Quantidade de acendimentos: " + lampada.qtdAcendimentos());
     lampada.click();
-    System.out.println("Após 2º click: " + lampada.checaEstado());
+    System.out.println("Apos segundo click: " + lampada.checaEstado());
     System.out.println("Quantidade de acendimentos: " + lampada.qtdAcendimentos());
 }
 }
